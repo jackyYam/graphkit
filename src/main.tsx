2251c6fe98431af82a0462c831db4e0221bbcae5
jackyYam <ziqi.yan@unholster.com>
2024-08-05 16:17:43 +0200

    Sentry configured

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import '@/lib/sentryConfig'
 import { StrictMode } from 'react'
 import * as ReactDOM from 'react-dom/client'
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,54 +1,67 @@
 /// <reference types='vitest' />
 import { defineConfig } from 'vite'
+import { loadEnv } from 'vite'
 import path from 'path'
 import react from '@vitejs/plugin-react'
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin'
 
-export default defineConfig({
-  root: __dirname,
-  cacheDir: './node_modules/.vite/graphkit',
+import { sentryVitePlugin } from '@sentry/vite-plugin'
 
-  server: {
-    port: 4200,
-    host: 'localhost',
-  },
-
-  preview: {
-    port: 4300,
-    host: 'localhost',
-  },
-
-  plugins: [react(), nxViteTsPaths()],
-
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-
-  build: {
-    outDir: './dist/graphkit',
-    emptyOutDir: true,
-    reportCompressedSize: true,
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
-  },
-
-  test: {
-    watch: false,
-    globals: true,
-    environment: 'jsdom',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-
-    reporters: ['default'],
-    coverage: {
-      reportsDirectory: './coverage/graphkit',
-      provider: 'v8',
-    },
-  },
+const sentryPlugin = sentryVitePlugin({
+  org: process.env.SENTRY_ORG,
+  project: process.env.SENTRY_PROJECT,
+  authToken: process.env.SENTRY_AUTH_TOKEN,
 })
+
+export default ({ mode }: { mode: string }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+  return defineConfig({
+    root: __dirname,
+    cacheDir: './node_modules/.vite/graphkit',
+
+    server: {
+      port: 4200,
+      host: 'localhost',
+    },
+
+    preview: {
+      port: 4300,
+      host: 'localhost',
+    },
+
+    plugins: [react(), nxViteTsPaths(), sentryPlugin],
+
+    // Uncomment this if you are using workers.
+    // worker: {
+    //  plugins: [ nxViteTsPaths() ],
+    // },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+
+    build: {
+      sourcemap: true,
+      outDir: './dist/graphkit',
+      emptyOutDir: true,
+      reportCompressedSize: true,
+      commonjsOptions: {
+        transformMixedEsModules: true,
+      },
+    },
+
+    test: {
+      watch: false,
+      globals: true,
+      environment: 'jsdom',
+      include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+
+      reporters: ['default'],
+      coverage: {
+        reportsDirectory: './coverage/graphkit',
+        provider: 'v8',
+      },
+    },
+  })
+}
